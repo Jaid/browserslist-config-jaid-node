7b72174eeaf059ca3d81653c7881e6c6122505bc
Jaid <jaid.jsx@gmail.com>
2019-07-08 15:08:53 +0200

    improve: Changed Webpack config for better compatibility with `browserslist`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,10 @@
 import configure from "webpack-config-jaid"
 
 export default configure({
-  documentation: true,
   publishimo: {fetchGithub: true},
+  terserOptions: false,
+  output: {
+    umdNamedDefine: false,
+    library: null,
+  },
 })
